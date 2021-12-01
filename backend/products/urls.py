3e197d19d08c4e22b2051aedaa7537970f6410d9
kumarmehta019 <kumarmehta019@gmail.com>
2021-12-01 17:44:35 +0000

    added crud

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
+from . import views
 
 urlpatterns = [
-    path('', ProductListView.as_view())
+    path('home/', views.home),
+    path('', ProductListView.as_view()),
+    path('<int:pk>/', ProductDetailView.as_view()),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -4,8 +4,7 @@ from rest_framework import status
 
 from .models import Product
 from .serializers import ProductSerializer
-
-# Create your views here.
+from django.http.response import HttpResponse
 
 
 class ProductListView(APIView):
@@ -17,3 +16,49 @@ class ProductListView(APIView):
         serialized_products = ProductSerializer(products, many=True)
         print('SERIALIZED PRODUCTS ->', serialized_products)
         return Response(serialized_products.data, status=status.HTTP_200_OK)
+
+    ## POST ROUTE
+    def post(self, request):
+        product = ProductSerializer(data=request.data)
+        # checks if it is valid
+        if product.is_valid():
+            # saves it to the DB
+            product.save()
+            # sends 201 back to the front-end
+            return Response(product.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(product.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+## CREATE, DELETE & UPDATE ROUTE
+class ProductDetailView(APIView):
+    ## GET BY ID
+    def get(self, _request, pk):
+        product = Product.objects.get(id=pk)
+        # converts python object into JSON
+        serialized_product = ProductSerializer(product)
+        return Response(serialized_product.data, status=status.HTTP_200_OK)
+
+    ## DELETE BY ID
+    def delete(self, request, pk):
+        try:
+            product = Product.objects.get(id=pk)
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTPP_500_INTERNAL_SERVER_ERROR)
+
+    ## UPDATE ROUTE
+    def put(self, request, pk):
+        product = Product.objects.get(id=pk)
+        updated_product = ProductSerializer(product, data=request.data)
+        if updated_product.is_valid():
+            updated_product.save()
+            return Response(updated_product.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(updated_product.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+# Create your views here.
+def home(request):
+    list_of_products = Product.objects.all()
+    print(list_of_products)
+    return HttpResponse('<h1>Hello</h1>')
