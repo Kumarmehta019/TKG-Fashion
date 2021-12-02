5e1a4c8e8c7259202f3d332940b33ce18ba7c4d8
Gayatri R <gayatrirajgor@gmail.com>
2021-12-02 15:50:48 +0000

    added options to category model

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+  CATEGORIES = (
+    ('T-Shirts', 'T-Shirts'),
+    ('Jeans', 'Jeans'),
+    ('Dresses', 'Dresses'),
+    ('Jumpers', 'Jumpers'), 
+    ('Shorts', 'Shorts'),
+    ('Shirts', 'Shirts'),
+    ('Trousers', 'Trousers'),
+    ('Socks', 'Socks')
+  )
+
   name = models.CharField(max_length = 50)
- 
 
   def __str__(self):
     return f'Category: {self.name}'
