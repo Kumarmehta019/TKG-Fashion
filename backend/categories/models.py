7e7f830a53d490aab27400033974aa3a58980bd6
Gayatri R <gayatrirajgor@gmail.com>
2021-12-02 15:12:25 +0000

    registered to admin and made changes to model

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -6,9 +6,19 @@ class Category(models.Model):
     ('M', 'Male'),
     ('F', 'Female')
   )
+  COLOURS = (
+    ('Black', 'Black'),
+    ('Yellow', 'Yellow'),
+    ('Blue', 'Blue'),
+    ('Red', 'Red'),
+    ('Green', 'Green'),
+    ('Orange', 'Orange'),
+    ('White', 'White')
+  )
   name = models.CharField(max_length = 50)
-  gender = models.CharField(max_length = 10, choices = GENDERS)
+  gender = models.CharField(max_length = 10, choices = GENDERS, blank = True)
+  colour = models.CharField(max_length = 10, choices = COLOURS, blank = True)
 
 
   def __str__(self):
-    return f'Category: {self.name}, Gender: {self.gender}'
+    return f'Category: {self.name}, Gender: {self.gender}, Colour: {self.colour}'
