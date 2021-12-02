fc527e98923b094c3b9b34b87d56e412c442a9d2
Gayatri R <gayatrirajgor@gmail.com>
2021-12-02 14:50:13 +0000

    added model

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  GENDERS = (
+    ('M', 'Male'),
+    ('F', 'Female')
+  )
+  name = models.CharField(max_length = 50)
+  gender = models.CharField(max_length = 10, choices = GENDERS)
+
+
+  def __str__(self):
+    return f'Category: {self.name}, Gender: {self.gender}'
