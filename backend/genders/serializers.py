e9e2c5cb11014ff3924b5564e41e9386ef08c38f
Gayatri R <gayatrirajgor@gmail.com>
2021-12-02 19:03:37 +0000

    added serializer

---
+++ backend/genders/serializers.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Gender
+
+class GenderSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Gender
+    fields = '__all__'
