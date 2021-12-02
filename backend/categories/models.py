965f0c7eef22594211082bd9e675426fcab24185
Gayatri R <gayatrirajgor@gmail.com>
2021-12-02 15:27:26 +0000

    made changes to category model

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -2,23 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-  GENDERS = (
-    ('M', 'Male'),
-    ('F', 'Female')
-  )
-  COLOURS = (
-    ('Black', 'Black'),
-    ('Yellow', 'Yellow'),
-    ('Blue', 'Blue'),
-    ('Red', 'Red'),
-    ('Green', 'Green'),
-    ('Orange', 'Orange'),
-    ('White', 'White')
-  )
   name = models.CharField(max_length = 50)
-  gender = models.CharField(max_length = 10, choices = GENDERS, blank = True)
-  colour = models.CharField(max_length = 10, choices = COLOURS, blank = True)
-
+ 
 
   def __str__(self):
-    return f'Category: {self.name}, Gender: {self.gender}, Colour: {self.colour}'
+    return f'Category: {self.name}'
--- backend/products/models.py
+++ backend/products/models.py
@@ -14,9 +14,9 @@ class Product(models.Model):
     size = models.CharField(max_length=10, default=None, choices=SIZES)
     stock = models.IntegerField()
     # image = models.CharField(max_length=500, default=None) -> make this another relationship (as Mike said)
-    # category(FK)
+    category = models.ForeignKey("categories.Category", on_delete = models.CASCADE)
 
     def __str__(self):
-        return f'{self.name, self.size}'
+        return f'Product: {self.name}, Size: {self.size}, Category: {self.category}'
     
 
