92083afe9be3f7666edbf8135ab7d427abfb517f
kumarmehta019 <kumarmehta019@gmail.com>
2021-12-03 11:19:49 +0000

    adding changes

--- backend/jwt_auth/models.py
+++ backend/jwt_auth/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     email = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
+    address = models.CharField(max_length=300)
 
 
     # django already has username and passwords (normal and confirmation)
