619ae894f500c70740399470e8d34a87ca3554ef
Gayatri R <gayatrirajgor@gmail.com>
2021-12-05 14:13:23 +0000

    working on the card and content

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import axios from 'axios'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ProductIndex from './components/ProductIndex'
 
 function App() {
-  React.useEffect(() => {
-    const getData = async () => {
-      const res = await axios.get('api/products') // * <-- replace with your endpoint
-      console.log(res.data)
-    }
-    getData()
-  })
 
   return (
     <>
-      {/* <h1>Hello World</h1> */}
       <BrowserRouter>
         <Routes>
           <Route exact path ='/browse' element={<ProductIndex/>} />
--- client/src/components/ProductIndex.js
+++ client/src/components/ProductIndex.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Card, Container } from 'semantic-ui-react'
 
 const ProductIndex = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get('api/products') // * <-- replace with your endpoint
+      console.log(data)
+      setProducts(data)
+    }
+    getData()
+  }, [])
+
+
+
   return (
-    <h1>Hello</h1>
+    <>
+      <Container>
+        <Card.Group>
+          {products.map(product => {
+            return (
+              <>
+                <Card key ={product.name}>
+                  <Card.Content>{product.name}</Card.Content>
+                  <Card.Content extra>£ {product.price}</Card.Content>
+                </Card>
+              </>
+            )
+          })}
+        </Card.Group>
+      </Container>
+    </>
   )
 }
 
