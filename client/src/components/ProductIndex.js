95a7d64bbf6732c16e9482591870a1b74da396b4
Gayatri R <gayatrirajgor@gmail.com>
2021-12-05 17:37:09 +0000

    working on filters

--- client/src/components/ProductIndex.js
+++ client/src/components/ProductIndex.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Card, Container } from 'semantic-ui-react'
+import { Card, Container, Dropdown, Grid, Header, Icon, Menu } from 'semantic-ui-react'
 
 const ProductIndex = () => {
 
@@ -20,18 +20,66 @@ const ProductIndex = () => {
   return (
     <>
       <Container>
-        <Card.Group>
-          {products.map(product => {
-            return (
-              <>
-                <Card key ={product.name}>
-                  <Card.Content>{product.name}</Card.Content>
-                  <Card.Content extra>£ {product.price}</Card.Content>
-                </Card>
-              </>
-            )
-          })}
-        </Card.Group>
+        <Header as='h1'>Browse our products</Header>
+        
+        <Grid columns='two'>
+          <Grid.Row>
+            <Grid.Column width={3} textAlign='left'>
+              <Container>
+                {/* <Grid.Column>
+                  <Header 
+                    as='h2'
+                    content='Filter'
+                    icon='filter'
+                  />
+                </Grid.Column> */}
+              </Container>
+              <Container>
+                <Menu text vertical>
+                  <Menu.Item header><Icon name='filter'/> Filter</Menu.Item>
+                  <Dropdown item multiple fluid text='Categories'>
+                    <Dropdown.Menu>
+                      <Dropdown.Item>T-Shirts</Dropdown.Item>
+                      <Dropdown.Item>Jumpers</Dropdown.Item>
+                      <Dropdown.Item>Dresses</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Menu>
+
+                <Menu text vertical>
+                  <Dropdown item fluid text='By Price'>
+                    <Dropdown.Menu>
+                      <Dropdown.Item>Low to High</Dropdown.Item>
+                      <Dropdown.Item>High to Low</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Menu>
+              </Container>
+
+            </Grid.Column>
+
+            <Grid.Column width={13}>
+              <Card.Group>
+                {products.map(product => {
+                  return (
+                    <>
+                      <Card key ={product.name}>
+                        <Card.Content>
+                          <Card.Header>Insert image here</Card.Header>
+                        </Card.Content>
+                        <Card.Content>{product.name}</Card.Content>
+                        <Card.Content extra>£ {product.price}</Card.Content>
+                      </Card>
+                    </>
+                  )
+                })}
+              </Card.Group>
+            </Grid.Column>
+
+          </Grid.Row>
+        </Grid>
+          
+        
       </Container>
     </>
   )
