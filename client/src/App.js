9d73afaa617dcc0e31c1c73bb7762a06ea05c423
kumarmehta019 <kumarmehta019@gmail.com>
2021-12-05 21:44:02 +0000

    adding authentication to navbar and adding profile and home page components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Switch } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
-
+import Home from './components/Home'
+import Profile from './components/Profile'
 
 
 const App = () => {
@@ -13,6 +14,8 @@ const App = () => {
       <NavBar />
       <div className="site-wrapper">
         <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/profile' component={Profile} />
         </Switch>
       </div>
       <Footer />
---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div id='home-section'>
+      <h1>HOMESCREEN TESTING</h1>
+    </div>
+  )
+}
+
+export default Home
