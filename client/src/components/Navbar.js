ec12e9c4a251c0dae8bfd89b491cff21aaa00001
Talia Glantz <taliaglantz13@gmail.com>
2021-12-06 09:55:03 +0000

    "merging navbars and auth"

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,67 +1,89 @@
-import React, { useEffect } from 'react'
-import { Menu, Dropdown, Icon } from 'semantic-ui-react'
-import Register from './Register'
-import Login from './Login'
-import { useNavigate, useLocation } from 'react-router-dom'
-import { getPayload } from './helpers/auth'
+import React, { useState, useEffect } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
+import { Container, Image, Menu, Icon, Dropdown, Modal, Header, Button } from 'semantic-ui-react'
+import { getPayload, getUsernameFromLocalStorage } from './helpers/auth'
 
 
-const Navbar = () => {
-  const navigate = useNavigate()
-  const location = useLocation() // returns an object containing a string of the pathname of users current location
+const NavBar = () => {
+  const [open, setOpen] = useState([])
+  const history = useHistory()
+  const location = useLocation()
 
   useEffect(() => {
 
-  }, [location.pathname]) // when location of user changes, trigger a rerender
+  }, [location.pathname])
 
   const userIsAuthenticated = () => {
-    const payload = getPayload() // get payload part of the token by calling get payload function
-    if (!payload) return false // if there is no payload returned function returns false
-    const now = Math.round(Date.now() / 1000) // get the current time in milliseconds and convert to seconds as this is the format the expiry time on the token is in
-    return now < payload.exp // check if the current time is less than the expiry time, returns a boolean
+    const payload = getPayload()
+    if (!payload) return false
+    const now = Math.round(Date.now() / 1000)
+    return now < payload.exp
   }
 
   const handleLogout = () => {
-    window.localStorage.removeItem('token') // remove token from local storage
-    navigate('/') // redirect user to the home page
+    window.localStorage.removeItem('token')
+    history.push('/')
   }
 
+  const username = getUsernameFromLocalStorage()
+
+
   return (
-    <Menu>
-      <Icon name='user' fitted size='large' />
-
-      <Dropdown>
-        <Dropdown.Menu>
-
-          {!userIsAuthenticated() ?
-            <>
-              <Dropdown.Item>
-                <Register />
-              </Dropdown.Item>
-
-              <Dropdown.Item>
-                <Login />
-              </Dropdown.Item>
-            </>
-            :
-            <>
-              <Dropdown.Item>
-                My Orders
-              </Dropdown.Item>
-            
-              <Dropdown.Item onClick={handleLogout}>
-                Logout
-              </Dropdown.Item>
-            </>
-          }
-        </Dropdown.Menu>
-      </Dropdown>
-
+    <Menu style={{ padding: '0px 50px' }} color='teal' secondary inverted size='large'>
+      <Container>
+        <Menu.Item header as='a' href='/' position='left'>
+          <Image style={{ marginRight: '10px' }} size='tiny' src='https://i.imgur.com/uOjwmDf.png' circular />
+          <h3 style={{ marginTop: 'unset', color: 'white' }}>TKG Fashion</h3>
+        </Menu.Item>
+        <Menu.Item as='a' href=''><Icon name='search' size='large' />Browse</Menu.Item>
+        
+        {!userIsAuthenticated() ?
+          <>
+            <Menu.Item position='right' as='a' href=''><Icon name='signup' size='large' />Register</Menu.Item>
+            <Menu.Item as='a' href=''><Icon name='user circle' size='large' />Sign In</Menu.Item>
+            <Menu.Item as='a' href=''><Icon name='shopping bag' size='large' />Bag</Menu.Item>
+          </>
+          :
+          <>
+            <Menu.Item as='a' href=''><Icon name='shopping bag' size='large' />Bag</Menu.Item>
+            <Menu.Item><Icon name='user secret' size='large' />
+              <Dropdown floating closeOnChange inline direction='left'>
+                <Dropdown.Menu size='mini'>
+                  <Dropdown.Header>Signed In / Register </Dropdown.Header>
+                  <Dropdown.Item as='a' href='/profile' icon='user circle' size='large' text={username} />
+                  <Modal
+                    basic
+                    onClose={() => setOpen(false)}
+                    onOpen={() => setOpen(true)}
+                    open={open}
+                    size='small'
+                    trigger={<Dropdown.Item as='a' onClick={handleLogout} icon='log out' text='Log Out' />}
+                    centered={true}
+                  >
+                    <Header icon>
+                      <Icon name='frown outline' />
+                      Logging Out?
+                    </Header>
+                    <Modal.Content>
+                      <p> Are you sure you want to log out, you will be missed?</p>
+                    </Modal.Content>
+                    <Modal.Actions>
+                      <Button basic color='red' onClick={() => setOpen(false)}>
+                        <Icon name='remove' /> No
+                      </Button>
+                      <Button basic color='green' onClick={() => setOpen(false)}>
+                        <Icon name='checkmark' /> Yes
+                      </Button>
+                    </Modal.Actions>
+                  </Modal>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Item>
+          </>
+        }
+      </Container>
     </Menu>
-
-
-
   )
 }
 
-export default Navbar
+export default NavBar
