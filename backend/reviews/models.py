e48f528c79f05ee3b575cd20098f727fba5bfc74
Talia Glantz <taliaglantz13@gmail.com>
2021-12-08 12:17:58 +0000

    finishing similar items

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -6,6 +6,7 @@ class Review(models.Model):
     rating = models.IntegerField()
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name='reviews')
     owner = models.ForeignKey('jwt_auth.User', on_delete=models.CASCADE, related_name='reviews')
+    
 
     def __str__(self):
         return f"Review: {self.rating}/5. {self.comment}"
--- client/src/components/ProductShow.js
+++ client/src/components/ProductShow.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useParams, Link } from 'react-router-dom'
-import { Grid, Image, Card, Divider, Header, Container, Comment, Segment, Button, Accordion, Icon } from 'semantic-ui-react'
+import { useParams } from 'react-router-dom'
+import { Grid, Image, Divider, Header, Container, Comment, Segment, Button, Accordion, Icon } from 'semantic-ui-react'
+import SimilarProducts from './SimilarProducts'
 
 
 const ProductShow = () => {
 
   const [product, setProduct] = useState([])
-  const [allProducts, setAllProducts] = useState([])
   const [category, setCategory] = useState([])
   const [hasError, setHasError] = useState(false)
   const { id } = useParams()
@@ -27,29 +27,9 @@ const ProductShow = () => {
       }
     }
     getData()
-  }, [id])
+  }, [id, product.category])
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const { data } = await axios.get('api/products')
-        console.log(data)
-        setAllProducts(data)
-      } catch (err) {
-        console.log(err)
-        setHasError(true)
-      }
-    }
-    getData()
-  }, [])
 
-  const filterByCategory = () => {
-
-    return allProducts.filter(product => {
-      return product.category === category
-    })
-
-  }
 
   const accordion = [
     {
@@ -103,76 +83,46 @@ const ProductShow = () => {
   // console.log(filteredProducts)
   return (
     <Container>
-      <Grid divided='vertically'>
-        <Grid.Row columns={2}>
-          <Grid.Column>
-            <Image src={product.image_set !== undefined ? product.image_set[0].image : null} />
-            {/* <Image size='large' src={product.image_set[0].image} /> */}
-          </Grid.Column>
-
-
-          <Grid.Column>
-            <section className='product-info-wrapper'>
-              <p className='product-name' textAlign='center'>{product.name}</p>
-              <p className='product-price'><Icon name='gbp' />{product.price}</p>
-              <Container>
-                <Segment compact inverted color={product.colour} />
-              </Container>
-              <br />
-              <div className='product-colour'>Colour: {product.colour}</div>
-              <div className='product-size'>Size: {product.size}</div>
-              <br />
-              <Button size='huge' color='teal'>Add to Bag</Button>
-              <br />
-              <br />
-              <Accordion defaultActiveIndex={0} panels={accordion} />
-            </section>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-
-      <Divider />
-
-      <Header as='h3'>You may also like...</Header>
-      {filterByCategory().length ?
+      {product ?
         <Container>
-          <Grid>
-            <Grid.Row columns={4}>
+          <Grid divided='vertically'>
+            <Grid.Row columns={2}>
+              <Grid.Column>
+                <Image src={product.image_set !== undefined ? product.image_set[0].image : null} />
+                {/* <Image size='large' src={product.image_set[0].image} /> */}
+              </Grid.Column>
 
-              {filterByCategory().map((product, index) => {
-                console.log(product)
-                console.log(product.name)
-                return (
-                  <>
-                    <Grid.Column>
-                      <Link key={index} to={`/${product.id}`}>
-                        <Card>
-                          <Image src={product.image_set !== undefined ? product.image_set[0].image : null} />
-                          <Card.Content>
-                            <Card.Header>{product.name}</Card.Header>
-                            <Card.Description>GBP £{product.price}</Card.Description>
-                          </Card.Content>
-                        </Card>
-                      </Link>
-                    </Grid.Column>
-                  </>
-                )
-              })}
-              
+
+              <Grid.Column>
+                <section className='product-info-wrapper'>
+                  <p className='product-name' textAlign='center'>{product.name}</p>
+                  <p className='product-price'><Icon name='gbp' />{product.price}</p>
+                  <Container>
+                    <Segment compact inverted color={product.colour} />
+                  </Container>
+                  <br />
+                  <div className='product-colour'>Colour: {product.colour}</div>
+                  <div className='product-size'>Size: {product.size}</div>
+                  <br />
+                  <Button size='huge' color='teal'>Add to Bag</Button>
+                  <br />
+                  <br />
+                  <Accordion defaultActiveIndex={0} panels={accordion} />
+                </section>
+              </Grid.Column>
             </Grid.Row>
           </Grid>
+
+          <Divider />
+
+          
+          <SimilarProducts category={ category } />
         </Container>
         :
-        <Header as='h3'>{hasError ? 'Looks like you&apos;ve grabbed the last gem! 💎 ' : 'Loading products... 👗 🩳 👚 '}</Header>
+        <Header as='h3'>{hasError ? 'Sorry, something has gone wrong 🚨 ' : 'Loading product 👗 🩳 👚 '}</Header>
       }
 
     </Container>
-
-
-
-
-
-
 
   )
 }
