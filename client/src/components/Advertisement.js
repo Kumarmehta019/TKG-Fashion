66e27fc65992308d78b38b30ddd56dd420ffc0b5
kumarmehta019 <kumarmehta019@gmail.com>
2021-12-08 09:45:44 +0000

    adding advertisement

---
+++ client/src/components/Advertisement.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Segment } from 'semantic-ui-react'
+
+const Advertisement = () => {
+  
+  return (
+    <Segment>
+      <Advertisement unit='billboard' test='Billboard' />
+    </Segment>
+  )
+}
+export default Advertisement
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Container } from 'semantic-ui-react'
 import Categories from './Categories'
 import HeroCarousel from './HeroCarousel'
+import Advertisement from './Advertisement'
 
 const Home = () => {
   return (
     <div id='home-section'>
       <Container style={{ background: 'black' }}>
         <HeroCarousel />
+      </Container>
+      <Container>
+        <Advertisement />
       </Container>
       <Container style={{ background: 'black' }}>
         <Categories />
