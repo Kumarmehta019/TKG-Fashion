38645690c06c6fae21bfd19fe33de7de6613d295
Gayatri R <gayatrirajgor@gmail.com>
2021-12-09 16:03:24 +0000

    populated orders

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import Order
+from products.serializers import PopulatedProductSerializer
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+
+class PopulatedOrderSerializer(OrderSerializer):
+    product = PopulatedProductSerializer(read_only=True)
