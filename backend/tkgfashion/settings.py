8039a7dacc6d1c875ad008c1d7a10623a0f901d7
Talia Glantz <taliaglantz13@gmail.com>
2021-12-09 09:45:18 +0000

    seeding reviews and users

--- backend/tkgfashion/settings.py
+++ backend/tkgfashion/settings.py
@@ -12,15 +12,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-)tt4@137vteotktn6r-!gli&e_gt=f0p@tzd0n^cvtck_8wq72'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
